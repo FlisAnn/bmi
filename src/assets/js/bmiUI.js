9f293d93b95959a8908132be7e777d56e687e07d
flis_ann <ann.flismark@gmail.com>
2020-10-14 12:06:56 +0200

    Adding ´´ that are missing in bmiUI file, thus fixing problems with tests

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -1,7 +1,7 @@
 const calculateBMI = () => {
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
-    let result = calculateMetricBMI(weightInput.nodeValue. heightInput.value)
+    let result = calculateMetricBMI(weightInput.value, heightInput.value)
     let displayResultElement = document.getElementById('result')
-    displayResultElement.innerHTML = <h1> Your BMI value is: ${result}</h1>
+    displayResultElement.innerHTML = `<h1> Your BMI value is: ${result}</h1>`
 }
