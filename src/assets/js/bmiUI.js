7fc920ae57153400ef0dc4edc4521c396599bc1f
flis_ann <ann.flismark@gmail.com>
2020-10-14 12:40:35 +0200

    Adding classification for BMI with if's and displaying the results

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -2,6 +2,7 @@ const calculateBMI = () => {
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
     let result = calculateMetricBMI(weightInput.value, heightInput.value)
+    let message = getBMIClassification(result)
     let displayResultElement = document.getElementById('result')
-    displayResultElement.innerHTML = `<h1> Your BMI value is: ${result}</h1>`
+    displayResultElement.innerHTML = `<h1> Your BMI value is: ${result} and you are ${message}</h1>`
 }
